a4740952ad15adcce84c87c56a9aec66fb314bd8
zodiakara <114649190+zodiakara@users.noreply.github.com>
2022-10-04 16:39:40 +0200

    Initial commit

---
+++ hw_pasta.js
@@ -0,0 +1,15 @@
+let spaghetti = 400;
+let guanciale = 250;
+let freshYolks = 120;
+let pecorino = 50;
+let bPepper = 4;
+let salt = 5;
+
+/* To make classic carbonara, first cut the guanciale into 1cm layers, then into long strips.
+Combine the egg yolks with the finely grated Pecorino Romano. */
+
+let cream =freshYolks+pecorino;
+let cookedPasta =spaghetti*1.8+salt;
+let final= cream + guanciale+cookedPasta;
+console.log ("weight of the dish is:", final)
+
---
+++ myname.js
@@ -0,0 +1,11 @@
+/*my first js file*/
+let myName ="Agata";
+let mySecondName="Julia";
+let myLastName="Orminska";
+let space=" ";
+
+console.log("Hey, my name is" + space+ myName+ space+mySecondName+ space + myLastName);
+
+let myAge =30;
+let currentYear=2022;
+console.log(currentYear - myAge);
